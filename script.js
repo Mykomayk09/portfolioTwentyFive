5622b1fc3bd299a4af2233c84a46eec8493581d5
Mykomayk09 <103438547+Mykomayk09@users.noreply.github.com>
2025-09-05 13:36:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.getElementById('year').textContent = currentYear;
 
 (function(){
   emailjs.init("A6DBNVW06kfSpH3tW");
-  });
+  })();
   document.getElementById("contact-form").addEventListener("submit", function(e) {
     e.preventDefault();
     emailjs.sendForm("service_u3lqth5", "template_ha0a0qw", this)
@@ -53,6 +53,7 @@ document.getElementById('year').textContent = currentYear;
 //element.classList.display=""}
 
 //hover underline in every title of section
+
 
 
 
